13cbe4ac389222200e9214a7d344b6855267f39b
aoki20160311 <sun@mbk.nifty.com>
2019-03-02 19:38:44 +0900

    表示対応

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,104 +1,30 @@
 package controllers
-import java.sql._
-
 import javax.inject._
-import play.api._
-import play.api.mvc._
-import play.api.data._
+
+
+import models._
+import play.api.data.Form
 import play.api.data.Forms._
-import play.api.db._
-import anorm._
-import PersonForm._
+import play.api.data.validation.Constraints._
+import play.api.i18n._
+import play.api.libs.json.Json
+import play.api.mvc._
+import scala.concurrent.{ExecutionContext, Future}
+
 
 @Singleton
-class HomeController @Inject()(db: Database, cc: MessagesControllerComponents)
+class HomeController @Inject()(repository: PersonRepository,
+                               cc: MessagesControllerComponents)(implicit ec: ExecutionContext)
   extends MessagesAbstractController(cc) {
 
-
-//  def index() = Action {implicit request =>
-//    var msg = "database record:<br><ul>"
-//    try {
-//      db.withConnection { conn =>
-//        val stmt = conn.createStatement
-//        val rs = stmt.executeQuery("SELECT * from people")
-//        while (rs.next) {
-//          msg += "<li>" + rs.getInt("id") + ":" + rs.getString("name") + "</li>"
-//        }
-//        msg += "</ul>"
-//      }
-//    } catch {
-//      case e:SQLException =>
-//        msg = "<li>no record...</li>"
-//    }
-//    Ok(views.html.index(
-//      msg
-//    ))
-
-  def index() = Action {implicit request =>
-    db.withConnection { implicit conn =>
-      val result:List[PersonForm.PersonData] =
-        SQL("Select * from people").as(personparser.*)
+  def index() = Action.async {implicit request =>
+    repository.list().map { people =>
       Ok(views.html.index(
-        "People Data.", result
+        "People Data.", people
       ))
-    }
-  }
-
-  def add() = Action {implicit request =>
-    Ok(views.html.add(
-      "フォームを記入して下さい。",
-      form
-    ))
-  }
-
-
-//  def create() = Action { implicit request =>
-//    val formdata = form.bindFromRequest
-//    val data = formdata.get
-//    try
-//      db.withConnection { conn =>
-//        val ps = conn.PreparedStatement(
-//          "insert into people values (default, ?, ?, ?)")
-//        ps.setString(1, data.name)
-//        ps.setString(2, data.mail)
-//        ps.setString(3, data.tel)
-//        ps.executeUpdate
-//      }
-//    catch {
-//      case e: SQLException =>
-//        Ok(views.html.add(
-//          "フォームに入力して下さい。",
-//          form
-//        ))
-//    }
-//    Redirect(routes.HomeController.index)
-//  }
-
-  def show(id:Int) = Action {implicit request =>
-    db.withConnection { implicit conn =>
-      val result:PersonData
-      = SQL("Select * from people where id = {id}")
-        .on("id" -> id)
-        .as(personparser.single)
-      Ok(views.html.show(
-        "People Data.", result
-      ))
-    }
-  }
-
-  def create() = Action { implicit request =>
-    val formdata = form.bindFromRequest
-    val data = formdata.get
-    db.withConnection { implicit conn =>
-      SQL("insert into people values (default, {name}, {mail}, {tel})")
-        .on(
-          "name" -> data.name,
-          "mail" -> data.mail,
-          "tel" -> data.tel
-        ).executeInsert()
-      Redirect(routes.HomeController.index)
     }
   }
 
 
 }
+
---
+++ app/models/PersonRepository.scala
@@ -0,0 +1,42 @@
+package models
+
+import javax.inject.{ Inject, Singleton }
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+
+
+import scala.concurrent.{ Future, ExecutionContext }
+
+
+@Singleton
+class PersonRepository @Inject()
+(dbConfigProvider: DatabaseConfigProvider)
+(implicit ec: ExecutionContext) {
+
+
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+
+  import dbConfig._
+  import profile.api._
+
+
+  private class PeopleTable(tag: Tag)
+    extends Table[Person](tag, "people") {
+    def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+    def mail = column[String]("mail")
+    def tel = column[Int]("tel")
+
+
+    def * = (id, name, mail, tel) <>
+      ((Person.apply _).tupled, Person.unapply)
+  }
+
+  def list(): Future[Seq[Person]] = db.run {
+    people.result
+  }
+
+
+  private val people = TableQuery[PeopleTable]
+}
