169b0ea8ccd94a5b3fe97f00862f732a77974533
aoki20160311 <sun@mbk.nifty.com>
2019-03-03 18:08:30 +0900

    変更

--- app/utilcalc/CalcDml.scala
+++ app/utilcalc/CalcDml.scala
@@ -1,9 +1,15 @@
 package utilcalc
 
+import scala.math.BigDecimal
+
+
 object CalcDml{
 
-  def CalcCv(tel: Int) {
+  def CalcCv(tel: Int): Double = {
 
+    val big1 = BigDecimal(tel)
+
+    return big1.asInstanceOf[Double]
 
   }
 }
