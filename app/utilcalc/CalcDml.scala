55f0e0155bf0f2df3b063ff8b830bd0f86349d77
k.aoki <sun@mbk.nifty.com>
2019-03-03 22:08:27 +0900

    四捨五入対応

--- app/utilcalc/CalcDml.scala
+++ app/utilcalc/CalcDml.scala
@@ -5,11 +5,14 @@ import scala.math.BigDecimal
 
 object CalcDml{
 
-  def CalcCv(tel: Int): Double = {
+  def CalcCv(tel: Int) = {
 
     val big1 = BigDecimal(tel)
 
-    return big1.asInstanceOf[Double]
+//    return big1.asInstanceOf[Double]
+    big1.setScale(1, scala.math.BigDecimal.RoundingMode.HALF_UP)
+
+//    val num3 = num3.setScale(4, scala.math.BigDecimal.RoundingMode.HALF_UP)
 
   }
 }
